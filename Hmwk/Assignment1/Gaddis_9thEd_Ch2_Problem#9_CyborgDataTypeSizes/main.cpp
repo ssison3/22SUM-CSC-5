68a457e84e2001d8435f62af253e2a37651a8ff7
Stephan S <109447586+ssison3@users.noreply.github.com>
2022-07-18 23:38:40 -0700

    Add files via upload

---
+++ Hmwk/Assignment1/Gaddis_9thEd_Ch2_Problem#9_CyborgDataTypeSizes/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Stephan S. Sison
+ * Created on June 29, 2022, 7:08 PM
+ * Purpose: Display information on the screen
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    cout<<"Char uses "<<sizeof(char)<<" byte"<<endl;
+    cout<<"Int uses "<<sizeof(int)<<" byte"<<endl;
+    cout<<"Float uses "<<sizeof(float)<<" byte"<<endl;
+    cout<<"Double uses "<<sizeof(double)<<" byte"<<endl;
+    
+    //Exit stage right
+    return 0;
+}
